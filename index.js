745f2ea0aac41d6b97ff3fd7ede239b85e15229c
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-13 19:55:43 -0600

    Setup index.js.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// code away!
+//////// Require ////////
+const express = require("express");
+const cors = require("cors");
+const logger =  require("./middleware/logger");
+
+//////// Routers ////////
+const usersRouter = require("./users/userRouter");
+const postsRouter = require("./posts/postRouter");
+
+//////// Server / Port ////////
+const server = express();
+const port = 3000
+
+//////// Servers / Middlewares ////////
+server.use(express.json());
+server.use(cors()):
+server.use(logger({ format: "long" }));
+
+server.use("/users", usersRouter);
+server.use("/users/:id/posts", postsRouter);
+
+server.use((req, res) => {
+    res
+        .status(404)
+        .json({
+            message: "Route was not found."
+        })
+});
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res
+        .status(500)
+        .json({
+            message: "Oops! Something went wrong!"
+        })
+});
+
+server.listen(port, () => {
+    console.log(`Server running on at http://localhost:${port}`);
+});
