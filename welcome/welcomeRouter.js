9d92bf038f5df63ae831f59e09fa8c62c0c90edd
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-16 20:13:55 -0600

    Fixed issue with new env variable in welcomeRouter.js.

--- welcome/welcomeRouter.js
+++ welcome/welcomeRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-        site_endpoints: process.env.SITE_ENDPOINTS
+        site_endpoints: process.env.SITE_ENDPOINTS,
 		message: `Welcome ${process.env.COHORT}`,
 		fun_fact_i_found_online: process.env.FUN_FACT || "I have no fun facts right now.",
 	})
